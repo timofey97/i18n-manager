92ba0d777b9ee32983598a2338edadcb3f05f1c0
Timofei Zagladkin <gleb285@gmail.com>
2025-04-10 16:58:46 +0200

    transform add

--- bin/i18n-manager.js
+++ bin/i18n-manager.js
@@ -4,6 +4,7 @@ const { program } = require('commander');
 const { createLanguageFile } = require('../lib/create-lang');
 const { syncTranslations } = require('../lib/sync');
 const { checkTranslations } = require('../lib/check');
+const { transformTranslations } = require('../lib/transform');
 const path = require('path');
 
 program.name('i18n-manager').description('A powerful i18n management tool for handling translations').version('1.0.0').option('-p, --path <path>', 'Path to translations directory', 'src/assets/i18n').option('-b, --base-lang <lang>', 'Base language code', 'en');
@@ -12,11 +13,10 @@ program
 	.command('create')
 	.description('Create a new language file')
 	.argument('<langCode>', 'Two-letter language code (e.g., fr, pl, id)')
-	.action((langCode) => {
-		const opts = program.opts();
-		const translationsPath = opts.path || 'src/assets/i18n';
-		const baseLang = opts.baseLang || 'en';
-		createLanguageFile(langCode, translationsPath, baseLang);
+	.option('-p, --path <path>', 'Path to translations directory', 'i18n')
+	.option('-b, --base-lang <lang>', 'Base language code', 'en')
+	.action((langCode, options) => {
+		createLanguageFile(langCode, options.path, options.baseLang);
 	});
 
 program
@@ -38,6 +38,15 @@ program
 		const translationsPath = opts.path || 'src/assets/i18n';
 		const baseLang = opts.baseLang || 'en';
 		checkTranslations(translationsPath, baseLang);
+	});
+
+program
+	.command('transform')
+	.description('Transform flat translation structure to nested format')
+	.option('-p, --path <path>', 'Path to translations directory', 'i18n')
+	.option('-l, --lang <lang>', 'Language code to transform (e.g., en, ru). If not specified, all languages will be transformed')
+	.action((options) => {
+		transformTranslations(options.path, options.lang);
 	});
 
 program.parse(); 
